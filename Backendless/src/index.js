cacc4d8e1868064d7ffb3bc2298781b937ef4a99
Nazarii <nazariipushkaruk@gmail.com>
2023-11-08 07:23:48 +0200

    Update index.js

--- Backendless/src/index.js
+++ Backendless/src/index.js
@@ -8,7 +8,7 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="Backendless-test-task">
+    <BrowserRouter basename="Backendless/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
